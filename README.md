c7d94440dfdcadaf239cf138d7843e7836f7f8d0
Vertigo NUC <rid090@csiro.au>
2019-11-05 17:31:36 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+```
+pip3 install -r requiremenets.txt
+python3 ./main.py
+```
---
+++ main.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO, send, emit
+import threading
+from time import sleep
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+socketio = SocketIO(app)
+
+@app.route('/')
+def hello_world():
+    return render_template("base.html")
+
+param_values = {
+    "seabed_height": 0.0,
+    "surface_depth": 0.0,
+    "track": 0.0,
+}
+
+@app.route('/annotation/videoPoint', methods=["POST"])
+def add_video_point():
+    content = request.json
+    print("videoPoint:" + str(content))
+    return jsonify(isError=False, message="Success", statusCode=200)
+
+@app.route('/annotation/videoRectangle', methods=["POST"])
+def add_video_rectangle():
+    content = request.json
+    print("videoRectangle:" + str(content))
+    return jsonify(isError=False, message="Success", statusCode=200)
+
+@app.route('/annotation/videoFrame', methods=["POST"])
+def add_video_frame():
+    content = request.json
+    print("videoFrame:" + str(content))
+    return jsonify(isError=False, message="Success", statusCode=200)
+
+@socketio.on('json', namespace="/control/mode")
+def control_mode(json):
+    "Handle change of mode controlled by frontend."
+    mode = json.get("mode")
+    if mode is None or mode not in ["surface", "seabed", "manual"]:
+        return
+
+    print("Mode set to " + mode)
+    socketio.emit("json", {"mode": mode}, namespace="/control/mode")
+
+@socketio.on('json', namespace="/session/state")
+def session_state(json):
+    "Handle change of session state controlled by frontend."
+    active = json.get("active")
+    paused = json.get("paused")
+
+    if type(active) != type(True) or type(paused) != type(True):
+        return
+
+    print("Session state set to active:" + str(active) + " paused:" + str(paused))
+    socketio.emit("json", {"active": active, "paused": paused}, namespace="/session/state")
+
+@socketio.on('json', namespace="/control/parameters")
+def control_parameters(json):
+    "Handle change of parameter setpoint by frontend."
+    if type(json) != type([]):
+        return
+    for param in json:
+        handle_parameter_change(param)
+
+def handle_parameter_change(param):
+    name = param.get("name")
+    value = param.get("value")
+
+    if name not in param_values.keys():
+        return
+
+    print("Parameter " + name + " changed to " + str(value))
+    param_values[name] = value
+
+def publish_function():
+    param_state = param_values.copy()
+
+    i = 0
+    while True:
+        for key in param_state.keys():
+            param_state[key] += (param_values[key] - param_state[key]) * 0.5
+
+        msg = []
+        for key in param_state.keys():
+            msg.append({
+                "name": key,
+                "value": param_state[key],
+                "unit": "m"
+            })
+        socketio.emit("json", msg, namespace="/control/parameters")
+        socketio.emit("json", [
+            {
+                "name": "depth",
+                "value": i,
+                "unit": "m"
+            },
+            {
+                "name": "depth",
+                "value": i/10,
+                "unit": "km"
+            }
+        ], namespace="/status/updates")
+        i += 0.4
+
+        sleep(1)
+
+if __name__ == '__main__':
+    t = threading.Thread(target=publish_function)
+    t.start()
+    socketio.run(app, host="0.0.0.0")
